965dbfdec45aa00d04883966721dfe027630b1cc
vadimkirov <vadimkirov@yandex.ru>
2019-10-10 20:40:50 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.candidate =  null;
     }
 
     guess() {
+       return  this.candidate = Math.round((this.max + this.min)/2);
 
     }
 
     lower() {
+        this.max = this.candidate;
 
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
